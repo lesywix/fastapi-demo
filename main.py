78cc62d640bde0b0360ef8446351951462422392
lesywix <lin.wx@foxmail.com>
2021-06-17 14:31:38 +0800

    Add test api

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import asyncio
+import time
 from typing import Optional
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import Cookie, Depends, FastAPI, HTTPException, Query
+from fastapi.exceptions import RequestValidationError
+from pydantic import BaseModel, EmailStr
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
+
+items = {"foo": "The Foo Wrestlers"}
 
 
 class Item(BaseModel):
@@ -13,7 +20,9 @@ class Item(BaseModel):
 
 
 @app.get("/")
-def read_root():
+async def read_root():
+    time.sleep(6)
+    # await asyncio.sleep(5)
     return {"Hello": "World"}
 
 
@@ -23,5 +32,79 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/items/")
+async def read_items(q: Optional[str] = Query(..., min_length=3, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items2/")
+async def read_items(ads_id=Cookie('test')):
+    return {"ads_id": ads_id}
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+    test: str = 'aaa'
+
+
+@app.post("/user/", response_model=UserOut, responses=None)
+async def create_user(user: UserIn):
+    return user
+
+
+@app.get("/test/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item": items[item_id]}
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
+
+
+@app.get("/itemtest/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
+
+
+class FixedContentQueryChecker:
+    def __init__(self, fixed_content: str):
+        self.fixed_content = fixed_content
+
+    def __call__(self, q: str = ""):
+        if q:
+            return self.fixed_content in q
+        return False
+
+
+checker = FixedContentQueryChecker("bar")
+
+
+@app.get("/query-checker/")
+async def read_query_check(fixed_content_included: bool = Depends(checker)):
+    return {"fixed_content_in_query": fixed_content_included}
